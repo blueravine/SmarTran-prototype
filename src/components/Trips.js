c3aee702664e72808d139352acd41daa603d1dff
imran-blueravine <imran.ali@blue-ravine.com>
2018-08-11 17:24:08 +0530

    updated trips and other
    updated trips and other

--- src/components/Trips.js
+++ src/components/Trips.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import MapView, { AnimatedRegion,Polyline,Marker } from 'react-native-maps';
+import MapView, { AnimatedRegion,Polyline,Marker, Callout, ProviderPropType } from 'react-native-maps';
 import { Image,ScrollView,StyleSheet,TouchableOpacity,StatusBar,
     TouchableHighlight,Dimensions,Animated,Easing } from 'react-native';
 import { Container, Header, Content, Card, CardItem, Thumbnail,Picker,DeckSwiper, Text,Item,Input,View,Fab,Icon, Button,  Left, Body, Right,
@@ -18,6 +18,13 @@ const cardItem = {cardItem: {fontSize: 40}};
 const { width } = Dimensions.get('window');
 const { height } = Dimensions.get('window');
 
+const ASPECT_RATIO = width / height;
+const LATITUDE = 17.4365557;
+const LONGITUDE = 78.3670722;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+import carImg from '../Images/car.png';
 
 export default class Trips extends Component {
 
@@ -30,16 +37,32 @@ export default class Trips extends Component {
         this.state= {
             active:'search',
         };
+        // this.state = {
+        //     coordinates: [
+        //         {
+        //             latitude: LATITUDE,
+        //             longitude: LONGITUDE,
+        //         },
+        //         {
+        //             latitude: 17.4338811,
+        //             longitude: 78.3702524,
+        //         },
+        //     ],
+        // };
+        //
+        // this.mapView = null;
+
 
         this.state = {
-            latitude: 37.78825,
-            longitude: -122.4324,
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
             routeCoordinates: [],
             distanceTravelled: 0,
             prevLatLng: {},
             coordinate: new AnimatedRegion({
-                latitude: 37.78825,
-                longitude: -122.4324,
+                latitude: LATITUDE,
+                longitude: LONGITUDE,
+
             })
         };
     }
@@ -71,11 +94,15 @@ export default class Trips extends Component {
                     distanceTravelled:
                     distanceTravelled + this.calcDistance(newCoordinate),
                     prevLatLng: newCoordinate
+
+
                 });
+                // Toast.show("Latitute"+this.state.latitude+"Longitude"+this.state.longitude);
             },
             error => console.log(error),
             { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
         );
+        // Toast.show("Latitute"+this.state.latitude+"Longitude"+this.state.longitude);
     }
     calcDistance = newLatLng => {
         const { prevLatLng } = this.state;
@@ -84,10 +111,63 @@ export default class Trips extends Component {
     getMapRegion = () => ({
         latitude: this.state.latitude,
         longitude: this.state.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: LATITUDE_DELTA,
+        longitudeDelta: LONGITUDE_DELTA,
+
     });
 
+    sleepabit (time) {
+        return new Promise((resolve) => setTimeout(resolve, time));
+    }
+
+    animate(){
+        let i;
+        i = 0;
+        let r = [
+            {
+                latitude : 17.4288048,
+                longitude : 78.3731846,
+                latitudeDelta : LATITUDE_DELTA,
+                longitudeDelta : LONGITUDE_DELTA,
+            },
+            {
+                latitude : 17.4288048,
+                longitude : 78.3731846,
+                latitudeDelta : LATITUDE_DELTA,
+                longitudeDelta : LONGITUDE_DELTA,
+            },
+            {
+                latitude : 17.4364795,
+                longitude : 78.364338,
+                latitudeDelta : LATITUDE_DELTA,
+                longitudeDelta : LONGITUDE_DELTA,
+            },
+
+        ];
+
+        // for(i=0;i<5;i++){
+
+        this.marker._component.animateMarkerToCoordinate(r[i] ,2000);
+        i=1;
+        this.marker._component.animateMarkerToCoordinate(r[i] ,2000);
+        i=2;
+        this.marker._component.animateMarkerToCoordinate(r[i] ,2000);
+        // this.marker._component.animateMarkerToCoordinate(r[i] ,2000);
+        i=0;
+        // this.marker._component.animateMarkerToCoordinate(r[i] ,2000);
+        i=1;
+        // this.marker._component.animateMarkerToCoordinate(r[i] ,2000);
+
+
+        // i>=2 ? i=0 : i;
+
+        // }
+
+
+
+        // this.mapView.animateToCoordinate(r ,2000);
+
+    }
     render() {
 
         return (
@@ -99,37 +179,69 @@ export default class Trips extends Component {
                         backgroundColor='#0c71b7'/>
                 </View>
                 {/*<ScrollView >*/}
-                <View style={[styles.headerview]}>
+                {/*<View style={[styles.headerview]}>*/}
 
-                    <MapView
-                        style={styles.map}
-                        showUserLocation
-                        followUserLocation
-                        loadingEnabled
-                        region={this.getMapRegion()}
+                <MapView
+                    ref = {(ref)=>this.mapView=ref}
+                    style={styles.map}
+                    showUserLocation
+                    followUserLocation
+                    loadingEnabled
+                    apikey={"AIzaSyD3a7smG62nUL0Wp4jsP4Iv3rNg763HFyQ"}
+                    // strokeWidth={3}
+                    mapType={"standard"}
+                    zoomEnabled={true}
+                    scrollEnabled={true}
+                    showsScale={true}
+                    zoomControlEnabled={true}
+                    maxZoomLevel={200}
+                    region={this.getMapRegion()}
+                >
+                    <MapView.Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} strokeColor='#2eacde' />
+                    <Marker.Animated
+                        ref={marker => {
+                            this.marker = marker;
+                        }}
+                        coordinate={this.state.coordinate}
+                        centerOffset={{ x: -18, y: -60 }}
+                        anchor={{ x: 0.69, y: 1 }}
+                        image={carImg}
+                        onPress={()=>this.animate()}
                     >
-                        <Polyline coordinates={this.state.routeCoordinates} strokeWidth={5} />
-                        <Marker.Animated
-                            ref={marker => {
-                                this.marker = marker;
-                            }}
-                            coordinate={this.state.coordinate}
-                        />
-                    </MapView>
+                    </Marker.Animated>
+                    {/*<Marker*/}
+                    {/*coordinate={{*/}
+                    {/*latitude: LATITUDE + SPACE,*/}
+                    {/*longitude: LONGITUDE + SPACE,*/}
+                    {/*}}*/}
+                    {/*centerOffset={{ x: -18, y: -60 }}*/}
+                    {/*anchor={{ x: 0.69, y: 1 }}*/}
+                    {/*image={carImg}*/}
+                    {/*/>*/}
+                    {/*<Marker*/}
+                    {/*coordinate={{*/}
+                    {/*latitude: LATITUDE - SPACE,*/}
+                    {/*longitude: LONGITUDE - SPACE,*/}
+                    {/*}}*/}
+                    {/*centerOffset={{ x: -42, y: -60 }}*/}
+                    {/*anchor={{ x: 0.84, y: 1 }}*/}
+                    {/*>*/}
+                    {/*<Callout>*/}
+                    {/*<View>*/}
+                    {/*<Text>This is a plain view</Text>*/}
+                    {/*</View>*/}
+                    {/*</Callout>*/}
+                    {/*</Marker>*/}
+                </MapView>
 
-                    <TouchableOpacity style={[styles.bubble, styles.button]}>
-                        <Text style={styles.bottomBarContent}>
-                            {parseFloat(this.state.distanceTravelled).toFixed(2)} km
-                        </Text>
-                    </TouchableOpacity>
-                </View>
+                {/*</View>*/}
                 {/*styles.bubble*/}
                 {/*<View style={styles.Container}>*/}
-                    {/*<TouchableOpacity style={[styles.bubble, styles.button]}>*/}
-                        {/*<Text style={styles.bottomBarContent}>*/}
-                            {/*{parseFloat(this.state.distanceTravelled).toFixed(2)} km*/}
-                        {/*</Text>*/}
-                    {/*</TouchableOpacity>*/}
+                {/*<TouchableOpacity style={[styles.bubble, styles.buttonContainer]}>*/}
+                {/*<Text style={styles.bottomBarContent}>*/}
+                {/*{parseFloat(this.state.distanceTravelled).toFixed(2)} km*/}
+                {/*</Text>*/}
+                {/*</TouchableOpacity>*/}
                 {/*</View>*/}
 
                 {/*</ScrollView>*/}
@@ -137,7 +249,7 @@ export default class Trips extends Component {
 
 
                 <View style={[styles.footer]}>
-                    <BottomNavigation active={'search'} hidden={false} >
+                    <BottomNavigation active={'trips'} hidden={false} >
                         <BottomNavigation.Action
                             key="search"
                             icon={<Image source={require('../Images/search_magnifier_black.png')} color="#669999" name="Search" style={{ width: 20, height: 20 }} />}
@@ -171,6 +283,7 @@ export default class Trips extends Component {
             </View>
 
         );
+        // Toast.show("Latitute"+this.state.latitude+"Longitude"+this.state.longitude);
     }
     // _renderContent = (Tab: string,) => {
     //     if(this.state.selectedTab==="Search"){
@@ -196,7 +309,7 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'space-between',
-        backgroundColor: '#0c71b7',
+        backgroundColor: '#FFFFFF',
 
     },
     headerview: {
@@ -205,7 +318,7 @@ const styles = StyleSheet.create({
         // borderWidth:5,
         // borderColor:'#917cb7',
         position: 'absolute',
-        backgroundColor: '#0c71b7',
+        backgroundColor: '#FFFFFF',
         paddingRight:5,
         paddingLeft:5,
         paddingTop:55,
@@ -230,29 +343,18 @@ const styles = StyleSheet.create({
     },
     box: {
 
-        backgroundColor: '#0c71b7',
+        backgroundColor: '#FFFFFF',
         // marginBottom: 10
         marginRight:5,
         marginLeft:5,
 
     },
-    // container: {
-    //     flex: 1,
-    //     justifyContent: 'center',
-    //     backgroundColor: '#F5FCFF',
-    // },
-    // title: {
-    //     textAlign: 'center',
-    //     fontSize: 22,
-    //     fontWeight: '300',
-    //     marginBottom: 20,
-    // },
     header: {
-        backgroundColor: '#0c71b7',
+        backgroundColor: '#FFFFFF',
         padding: 10,
         borderTopEndRadius:5,
         borderWidth:2,
-        // borderColor:'#0C71B7',
+        // borderColor:'#FFFFFF',
         marginRight:5,
         marginLeft:5,
     },
@@ -260,69 +362,33 @@ const styles = StyleSheet.create({
         // textAlign: 'center',
         fontSize: 16,
         fontWeight: '500',
-        // color:'#0C71B7',
+        // color:'#FFFFFF',
     },
     content: {
         padding: 20,
-        backgroundColor: '#0c71b7',
+        backgroundColor: '#FFFFFF',
         // color:'#B7B152',
         marginRight:5,
         marginLeft:5,
     },
 
-
-
-    wrapper: {
-    },
-    slide1: {
-        flex: 1,
-        justifyContent: 'center',
+    Container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
         alignItems: 'center',
-        backgroundColor: '#0c71b7',
     },
-    slide2: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#0c71b7',
+    map: {
+        ...StyleSheet.absoluteFillObject,
     },
-    slide3: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#0c71b7',
+    bubble: {
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20,
     },
-    text: {
-        color: '#FFFFFF',
-        fontSize: 14,
-        fontStyle:'italic',
-        fontWeight:'bold'
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        backgroundColor: 'transparent',
     },
-    image: {
-        width,
-        flex: 1
-    }
-    // active: {
-    //     backgroundColor: 'rgba(255,255,255,1)',
-    // },
-    // inactive: {
-    //     backgroundColor: 'rgba(245,252,255,1)',
-    // },
-    // selectors: {
-    //     marginBottom: 10,
-    //     flexDirection: 'row',
-    //     justifyContent: 'center',
-    // },
-    // selector: {
-    //     backgroundColor: '#F5FCFF',
-    //     padding: 10,
-    // },
-    // activeSelector: {
-    //     fontWeight: 'bold',
-    // },
-    // selectTitle: {
-    //     fontSize: 14,
-    //     fontWeight: '500',
-    //     padding: 10,
-    // },
 });
